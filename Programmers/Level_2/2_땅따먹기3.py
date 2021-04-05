47a1d8931efdaff940ee977d61640da6f32a5786
jisupark <parkjisu6239@gmail.com>
2021-04-05 23:24:35 +0900

    2021-04-05

--- Programmers/Level_2/2_땅따먹기3.py
+++ Programmers/Level_2/2_땅따먹기3.py
@@ -1,9 +1,20 @@
 def solution(land):
-    sel = []
-    for i in range(len(land)):
-        max_num = 0
-        for j in range(len(land[0])):
-            pass
-    return 0
+    #print(land)
+
+    # 땅을 계속 현재행+이전행 선택결과의 최대값으로 갱신
+    # 현재 행에서는 이전에 밟은거랑 같은 열이 아니고, 최대가 되게끔
+    # 그리디 너낌..? 현재행에서 할수 있는 최대값만 찾는 방법
+
+    N = len(land)
+    for i in range(1, N):
+        land[i][0] = land[i][0] + max(land[i - 1][1], land[i - 1][2], land[i - 1][3])
+        land[i][1] = land[i][1] + max(land[i - 1][0], land[i - 1][2], land[i - 1][3])
+        land[i][2] = land[i][2] + max(land[i - 1][0], land[i - 1][1], land[i - 1][3])
+        land[i][3] = land[i][3] + max(land[i - 1][0], land[i - 1][1], land[i - 1][2])
+
+    #print(land)
+
+    return max(land[N-1])
+
 
 print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
