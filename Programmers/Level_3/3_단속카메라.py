4aa3e5f54cbe672ca6ff20fbedff4660ad3518c6
jisupark <parkjisu6239@gmail.com>
2021-04-17 20:31:23 +0900

    단속카메라

--- Programmers/Level_3/3_단속카메라.py
+++ Programmers/Level_3/3_단속카메라.py
@@ -1,8 +1,14 @@
 def solution(routes):
-    highway = dict()
-    for route in routes:
-        for i in range(route[0], route[1] + 1):
-            highway[i] = highway.get(i, 0) + 1
+    routes.sort(key = lambda x : (x[1]))
+    camera = routes[0][1]
+    cnt = 1
+    for i, o in routes:
+        if i <= camera <= o:
+            continue
+        else:
+            cnt += 1
+            camera = o
 
+    return cnt
 
 print(solution([[-20,15], [-14,-5], [-18,-13], [-5,-3]]))
