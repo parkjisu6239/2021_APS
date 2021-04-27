998c94a8bbd9509ed4c6813bec68b05c2f95e36c
jisupark <parkjisu6239@gmail.com>
2021-04-27 23:02:05 +0900

    2021-04-27

---
+++ BJ/알고리즘분류/DP/9095_123더하기/s1.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+def DP(N):
+    if N < len(memo):
+        return memo[N]
+    memo.append(DP(N-1) + DP(N-2) + DP(N-3))
+    return memo[N]
+
+for tc in range(1, int(input())+1):
+    N = int(input())
+    memo = [0, 1, 2, 4]
+    print(DP(N))
+
