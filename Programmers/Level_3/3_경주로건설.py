21528f65d3b23f5d0e7b7771be6486a8ad9d3581
jisupark <parkjisu6239@gmail.com>
2021-05-19 19:47:23 +0900

    2021-05-19

--- Programmers/Level_3/3_경주로건설.py
+++ Programmers/Level_3/3_경주로건설.py
@@ -1,5 +1,4 @@
 def solution(board):
-    answer = 0
     N = len(board)
     cost = [[987654321] * N for _ in range(N)]
 
@@ -9,7 +8,7 @@ def solution(board):
     dc = [0, 0, -1, 1]
 
     que = [start]
-    cost[0][0] = 100
+    cost[0][0] = 0
 
     while que:
         r, c, direction = que.pop(0)
@@ -18,19 +17,19 @@ def solution(board):
             nr, nc = r + dr[k], c + dc[k]
             if 0 <= nr < N and 0 <= nc < N and board[nr][nc] == 0:
                 if (r, c) == (0, 0) or direction == k:
-                    if cost[r][c]+100 < cost[nr][nc]:
+                    if cost[r][c]+100 <= cost[nr][nc]:
                         que.append((nr, nc, k))
                         cost[nr][nc] = cost[r][c]+100
                 else:
-                    if cost[r][c]+500 < cost[nr][nc]:
+                    if cost[r][c]+600 <= cost[nr][nc]:
                         que.append((nr, nc, k))
-                        cost[nr][nc] = cost[r][c]+500
+                        cost[nr][nc] = cost[r][c]+600
 
     for cos in cost:
         print(cos)
     return cost[N-1][N-1]
 
-print(solution([[0,0,0],[0,0,0],[0,0,0]]), 900)
-print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]), 3800)
-print(solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]]), 2100)
+# print(solution([[0,0,0],[0,0,0],[0,0,0]]), 900)
+# print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]), 3800)
+# print(solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]]), 2100)
 print(solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]), 3200)
