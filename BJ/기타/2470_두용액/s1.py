fb6ae338752907fe521cba890985a6ceb497b898
jisupark <parkjisu6239@gmail.com>
2021-05-25 23:17:36 +0900

    boj 2470 two liquid

---
+++ BJ/기타/2470_두용액/s1.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+N = int(input())
+potion = list(map(int, input().split()))
+potion.sort()
+
+s, e = 0, N-1
+result = [ abs(potion[e] + potion[s]), s, e]
+
+while s < e:
+    if result[0] == 0:
+        break
+
+    mix = abs(potion[e] + potion[s]) # 새로 섞은게 최소이면
+    if mix < result[0]: # 최소값 갱신
+        result = [mix, s, e]
+
+    # 앞,뒤 움직인 값
+    s_move = abs(potion[s+1] + potion[e])
+    e_move = abs(potion[s] + potion[e-1])
+
+    if s + 1 == e: # 한개차이면 끝
+        break
+
+    if s_move < e_move:
+        s += 1
+    else:
+        e -= 1
+
+print(potion[result[1]], potion[result[2]])
