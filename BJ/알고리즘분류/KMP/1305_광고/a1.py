329420358eb92d8fca46b8a8c0fccc8197f10237
parkjisu6239 <parkjisu6239@gmail.com>
2021-08-29 21:08:38 +0900

    [boj | KMP] 1305 광고

---
+++ BJ/알고리즘분류/KMP/1305_광고/a1.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('input.txt')
+
+def kmp(content):
+    n = len(content)
+    table = [0] * n
+    j = 0
+    for i in range(1, n):
+        while j > 0 and content[i] != content[j]:
+            j = table[j-1]
+
+        if content[i] == content[j]:
+            j += 1
+            table[i] = j
+
+    return table
+
+
+L = int(sys.stdin.readline())
+ad = sys.stdin.readline().rstrip()
+
+print(L - kmp(ad)[-1])
