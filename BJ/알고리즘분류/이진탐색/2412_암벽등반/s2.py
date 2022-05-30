a66f2c7baf508ae68ee0f9d8a3f62e115eb7c497
parkjisu6239 <parkjisu6239@gmail.com>
2022-05-30 21:39:07 +0900

    boj fix

--- BJ/알고리즘분류/이진탐색/2412_암벽등반/s2.py
+++ BJ/알고리즘분류/이진탐색/2412_암벽등반/s2.py
@@ -6,20 +6,21 @@ input = sys.stdin.readline
 
 
 N, T = map(int, input().split())
-visited = defaultdict(list)
+visited = defaultdict(set)
 for _ in range(N):
-    x,y = tuple(map(int, input().split()))
-    visited[x].append(y)
-que = deque([[0,0,0]])
-while que :
-    x,y,cnt = que.popleft()
+    x, y = tuple(map(int, input().split()))
+    visited[x].add(y)
+que = deque([[0, 0, 0]])
+
+while que:
+    x, y, cnt = que.popleft()
     if y == T:
         print(cnt)
         break
-    for xx in range(max(0, x-2),x+3,1):
-        for yy in range(max(0, y-2),y+3,1):
-            if yy in visited[xx] :
+    for xx in range(max(0, x-2), x+3, 1):
+        for yy in range(max(0, y-2), y+3, 1):
+            if yy in visited[xx]:
                 visited[xx].remove(yy)
-                que.append([xx,yy,cnt+1])
+                que.append([xx, yy, cnt+1])
 else:
     print(-1)
--- BJ/알고리즘분류/이진탐색/3079_입국심사/s1.py
+++ BJ/알고리즘분류/이진탐색/3079_입국심사/s1.py
@@ -5,8 +5,8 @@ input = sys.stdin.readline
 
 # 대기시간 생각할 필요 X
 # 심사하는데 x 시간이 걸리는 심사관이 t 시간 동안 심사할 수 있는 사람 수는 ? t // x
-# 1️⃣ t 시간동안 각 심사관이 심사할 수 있는 사람의 합 ? sum(x1//t +  x2//t + ... + xn//t)
-# 2️⃣ 총 M 명을 t 시간동안 심사할 수 있는가 ? sum(x1//t +  x2//t + ... + xn//t) >= M
+# 1️⃣ t 시간동안 각 심사관이 심사할 수 있는 사람의 합 ? sum(t//x1 +  t//x2 + ... + t//xn)
+# 2️⃣ 총 M 명을 t 시간동안 심사할 수 있는가 ? sum(t//x1 +  t//x2 + ... + t//xn) >= M
 
 N, M = map(int, input().split())
 waiting = [int(input()) for _ in range(N)]
